4ae37fb2113dd0efe621055ec2ee52708c48cd8c
Steve Hudek <sjhudek@gmail.com>
2023-04-09 13:34:59 -0600

    Incoporate arrays and for loops

---
+++ assets/script.js
@@ -0,0 +1,20 @@
+// const topic = "HTML"
+
+// if (topic === 'HTML') {
+//   console.log("Let's study HTML!")
+// } else if (topic === 'CSS') {
+//   console.log("Let's study CSS!")
+// } else if (topic === 'Git') {
+//   console.log("Let's study Git!")
+// } else if (topic === 'JavaScript') {
+//   console.log("Let's study JavaScript!")
+// } else {
+//   console.log('Please try again!')
+// }
+
+const shapes = ['HTML', 'CSS', 'Git', 'JavaScript']
+
+for (i= 0; i < shapes.length; i++) {
+    console.log(shapes[i])
+}
+
